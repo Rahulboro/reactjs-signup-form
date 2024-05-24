5ebf49a62d424b7f3770037e1c8ffa9eb4443bc8
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2024-05-24 23:54:17 +0530

    made changes in the login button and gave the functionallity of switchin

--- src/Components/Loginsignup/LoginSignUp.jsx
+++ src/Components/Loginsignup/LoginSignUp.jsx
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import usericon from "../Assets/user.png";
 import mail from "../Assets/gmail.png";
 import password from "../Assets/lock.png";
 
 const LoginSignUp = () => {
+  const [action, setaction] = useState("sign up");
   return (
     <div className="container">
       <div className="header">
-        <div className="text">Sign Up</div>
+        <div className="text">{action}</div>
         <div className="underline"></div>
       </div>
       <div className="inputs">
-        <div className="input">
-          <img src={usericon} alt="" />
-          <input id="text-input" type="text" placeholder="Name" />
-        </div>
+        {action === "Login" ? (
+          <div></div>
+        ) : (
+          <div className="input">
+            <img src={usericon} alt="" />
+            <input id="text-input" type="text" placeholder="Name" />
+          </div>
+        )}
+
         <div className="input">
           <img src={mail} alt="" />
           <input id="email-input" type="email" placeholder="Email" />
@@ -25,12 +31,31 @@ const LoginSignUp = () => {
           <input id="password-input" type="password" placeholder="Password" />
         </div>
       </div>
-      <div className="forget-password">
-        Forgot Password <span>Click here</span>
-      </div>
+      {action === "Login" ? (
+        <div></div>
+      ) : (
+        <div className="forget-password">
+          Forgot Password <span>Click here</span>
+        </div>
+      )}
+
       <div className="submitbtn">
-        <div className="submit">Sign Up</div>
-        <div className="submit">Login</div>
+        <div
+          className={action === "Login" ? "submit gray" : "submit"}
+          onClick={() => {
+            setaction("Sign Up");
+          }}
+        >
+          Sign Up
+        </div>
+        <div
+          className={action === "Sign Up" ? "submit gray" : "submit"}
+          onClick={() => {
+            setaction("Login");
+          }}
+        >
+          Login
+        </div>
       </div>
     </div>
   );
